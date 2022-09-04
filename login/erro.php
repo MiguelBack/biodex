c9d13be0649bc369f225af8df1bf914079210df1
MiguelBack <88088642+MiguelBack@users.noreply.github.com>
2022-09-04 17:47:57 -0300

    visual-definido

---
+++ login/erro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Sistema de Vendas</title>
+</head>
+
+<body>
+
+    <p>Algum erro aconteceu!</p>
+
+    <p> <a href="/front/public/index/index.html"> Voltar </a> </p>
+
+</body>
+
+</html>
